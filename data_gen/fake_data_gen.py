1c10c44f4d77ea8fb4f9bdf46ad5df749d931c54
Peter Ptroc <peterptroc@gmail.com>
2024-12-05 23:37:47 +0800

    [feature] 增加了数据生成脚本

---
+++ data_gen/fake_data_gen.py
@@ -0,0 +1,35 @@
+from faker import Faker
+import random
+
+fake = Faker('zh_CN')
+
+data_size = 500
+
+def generate_student_data():
+    # 学号
+    num = '2024' + str(random.randint(1000, 9999))
+    
+    # 姓名
+    name = fake.name()
+    
+    # 班级
+    class_num = '24' + str(random.randint(1, 15)).zfill(2)
+    
+    # 成绩&绩点
+    scores_credits = []
+    for _ in range(10):
+        score = random.choices(
+            population=[random.randint(61, 100), random.randint(0, 60)],
+            weights=[0.9, 0.1]
+        )[0]
+        credit = round(random.uniform(0.5, 5.0), 1)
+        scores_credits.append(f"{score} {credit}")
+    
+    # 拼接
+    student_data = f"{num} {name} {class_num} " + " ".join(scores_credits)
+    return student_data
+
+# 导出
+with open('export.ini', 'w') as file:
+    for _ in range(data_size):
+        file.write(generate_student_data() + '\n')
